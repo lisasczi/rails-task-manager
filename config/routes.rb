f7d5e64ad273b3d6f187a1393651efe33d2aeb45
lisa scanzi <lisascanzi@gmail.com>
2022-05-17 15:29:39 +0200

    exo tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # First, add a new route to list the tasks, following the convention from the lecture.
-  # Then, add a controller action and its view.
-  get 'list', to: 'tasks#list'
-  # This action should fetch all tasks, and a view should loop over these to display them,like in the screenshot below.
+  get 'index', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update", as: :tasks_update
+  delete "tasks/:id", to: "tasks#destroy"
 end
